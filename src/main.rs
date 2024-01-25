6c27a86a4b2a840f6e1419c0836171b2d2518c51
Andrei <andrei.i@posteo.de>
2024-01-25 00:00:00 +0000

    Add investigative findings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod recipient;
+
+use crate::recipient::{RecipientDecoder, RecipientNode, ServiceKind};
+
 use anyhow::{anyhow, Result};
 use colored::Colorize;
 use lightning_invoice::{Bolt11Invoice, RouteHint};
@@ -25,10 +29,16 @@ fn main() -> Result<()> {
     let node_name = graph_database.query_name(pubkey.clone())?;
     let route_hints = process_route_hints(&invoice.route_hints(), &graph_database)?;
 
-    println!("🕵️‍  Detective investigative findings:");
-    println!("🧾 Evidences:");
+    println!("🕵️‍  {}", " Investigative findings ".reversed());
+    let recipient_decoder = RecipientDecoder::new();
+    let recipient = recipient_decoder.decode(&pubkey, &route_hints);
+    let recipient = format_recipient_node(&recipient);
+    println!("   Recipient: {recipient}");
+
+    println!();
+    println!("🧾 {}", " Evidences ".reversed());
     println!(
-        "  Pay {} to {}",
+        "   Pay {} to {}",
         amount.bold(),
         format_node_name(&node_name)
     );
@@ -38,24 +48,67 @@ fn main() -> Result<()> {
             .map(format_node_name)
             .collect::<Vec<_>>()
             .join(" → ");
-        println!("    via {hint}");
+        println!("     via {hint}");
     }
     if !description.is_empty() {
-        println!("  Description: {}", description.italic());
+        println!("   Description: {}", description.italic());
     }
     Ok(())
 }
 
+fn format_service_kind(service: &ServiceKind) -> &str {
+    match service {
+        ServiceKind::BusinessWallet => "Payment processor",
+        ServiceKind::ConsumerWallet => "Consumer wallet",
+        ServiceKind::Exchange => "Exchange",
+        ServiceKind::Lsp => "LSP",
+    }
+}
+
+fn format_recipient_node(node: &RecipientNode) -> String {
+    match node {
+        RecipientNode::Custodial { custodian } => format!(
+            "Custodial {} {}",
+            format_service_kind(&custodian.service),
+            custodian.name.bold()
+        ),
+        RecipientNode::NonCustodial { id, lsp } => format!(
+            "Non-custodial {} {} with id:{}",
+            format_service_kind(&lsp.service),
+            lsp.name.bold(),
+            id.bold()
+        ),
+        RecipientNode::NonCustodialWrapped { lsp } => {
+            format!(
+                "Non-custodial {} {}",
+                format_service_kind(&lsp.service),
+                lsp.name.bold()
+            )
+        }
+        RecipientNode::Unknown => "Unknown".to_string(),
+    }
+}
+
 #[derive(Clone)]
 enum NodeName {
-    PublicNodeAlias(String),
+    PublicNodeAlias(String, String),
     PublicNodePubkey(String),
     PrivateNodePubkey(String),
 }
 
+impl NodeName {
+    fn get_alias(&self) -> &String {
+        match self {
+            NodeName::PublicNodeAlias(_alias, pubkey) => pubkey,
+            NodeName::PublicNodePubkey(pubkey) => pubkey,
+            NodeName::PrivateNodePubkey(pubkey) => pubkey,
+        }
+    }
+}
+
 fn format_node_name(node_name: &NodeName) -> String {
     match node_name {
-        NodeName::PublicNodeAlias(alias) => format!("node alias:{}", alias.bold()),
+        NodeName::PublicNodeAlias(alias, _) => format!("public node alias:{}", alias.bold()),
         NodeName::PublicNodePubkey(pubkey) => format!("public node id:{}", pubkey.bold()),
         NodeName::PrivateNodePubkey(pubkey) => format!("private node id:{}", pubkey.bold()),
     }
@@ -89,7 +142,7 @@ impl GraphDatabase {
 
     fn query_name(&self, pubkey: String) -> Result<NodeName> {
         let node_name = match self.query_alias(&pubkey)? {
-            Some(alias) if !alias.is_empty() => NodeName::PublicNodeAlias(alias),
+            Some(alias) if !alias.is_empty() => NodeName::PublicNodeAlias(alias, pubkey),
             Some(pubkey) => NodeName::PublicNodePubkey(pubkey),
             None => NodeName::PrivateNodePubkey(pubkey),
         };
